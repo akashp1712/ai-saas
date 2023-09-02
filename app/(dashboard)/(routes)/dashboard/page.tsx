eae5321b7b104ecfa67a5e93134e341f24788c79
Akash Panchal <akash@workbook.local>
2023-09-02 21:50:09 +0530

    Folder setup & Clerk intefgration

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>Dashboard Page (Protected)</p>
+      <UserButton afterSignOutUrl='/'/>
+    </div>
+    
+  );
+}
+
+export default DashboardPage;
