b17b81285cfdf9f647b31b864cc0bdeee0404333
Akash Panchal <akash@workbook.local>
2023-09-03 10:33:26 +0530

    Added Dashboard page, Sidebar and Mobile-Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import { UserButton } from '@clerk/nextjs'
 
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page (Protected)</p>
-      <UserButton afterSignOutUrl='/'/>
-    </div>
-    
+    </div> 
   );
 }
 
