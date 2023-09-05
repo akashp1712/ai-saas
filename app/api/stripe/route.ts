fd2be2378903ec645d7bb8b2284a47e100502d4d
Akash Panchal <akash@workbook.local>
2023-09-05 17:27:43 +0530

    Stripe Integration 1: Stripe checkout page
     1. Added stripe package and created lib for stripe
     2. Created new business in stripe and copies private key
     3. Added prisma schema for stripe subscription and updated the db using command: npx prisma db push
     4. Added util function to generate absolute url
     5. Added route for stripe checkout page

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,70 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+
+const settingsUrl = absoluteUrl("/settings");
+
+export async function GET() {
+    try {
+        const { userId } = auth();
+        const user = await currentUser();
+
+        if (!userId || !user) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const userSubscription = await prismadb.userSubscription.findUnique({
+            where: {
+                userId
+            }
+        });
+
+        // we don't want to create a checkout page, we want to redirect user to the billing page so they can cancel the subscription
+        if (userSubscription && userSubscription.stripeCustomerId) {
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: userSubscription.stripeCustomerId,
+                return_url: settingsUrl
+            });
+
+            return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+        }
+
+        // if we don't have a stripe subscription, we will create  a checkout session
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: settingsUrl,
+            cancel_url: settingsUrl,
+            payment_method_types: ["card"],
+            mode: "subscription",
+            billing_address_collection: "auto",
+            customer_email: user.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price_data: {
+                        currency: "USD",
+                        product_data: {
+                            name: "Genius Pro",
+                            description: "Unlimited AI Generations",
+                        },
+                        unit_amount: 2000,
+                        recurring: {
+                            interval: "month"
+                        }
+                    },
+                    quantity: 1,
+                }
+            ],
+            metadata: {
+                 userId, // this is important as stripe returns it to us
+            }
+        })
+
+        return new NextResponse(JSON.stringify({ url: stripeSession.url}));
+
+    } catch (error) {
+        console.log("{STRIPE_ERROR]", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
